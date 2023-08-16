5dafb0e499941ed1fad1c13741aa384575c38a24
[SW5] 원종석 <eonjeongcha13@gmail.com>
2023-08-16 19:50:52 +0900

    feat: 라우팅 및 global css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,54 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
+import { GlobalStyle } from "@/App.styles";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "@/components/layout/Header/Header";
+import Footer from "@/components/layout/Footer/Footer";
+import Todo from "@/pages/Todo/Todo";
+import Login from "@/pages/Login/Login";
+// import Ranking from "@/pages/Ranking/Ranking";
+// import Pet from "@/pages/Pet/Pet";
+// import MyPage from "@/pages/MyPage/MyPage";
+// import NotFound from "@/pages/NotFound";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>따뜻한 얼음</p>
-            </header>
-        </div>
+        <>
+            <GlobalStyle />
+            <Router>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <LayoutWithHeaderAndFooter
+                                setLayout={false}
+                                children={<Login />}
+                            />
+                        }
+                    />
+                    <Route
+                        path="/todo"
+                        element={
+                            <LayoutWithHeaderAndFooter
+                                setLayout={true}
+                                children={<Todo />}
+                            />
+                        }
+                    />
+                </Routes>
+            </Router>
+        </>
+    );
+}
+
+// @ts-ignore
+function LayoutWithHeaderAndFooter({ setLayout, children }) {
+    return (
+        <>
+            {setLayout && <Header />}
+            {children}
+            {setLayout && <Footer />}
+        </>
     );
 }
 
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,23 @@
+import styled from "styled-components";
+
+import mainImage from "@/assets/images/main.png";
+
 export default function Login() {
-  return (
-    <div>
-      <div>Login</div>
-    </div>
-  );
+    return (
+        <Container>
+            <img src={mainImage} alt="main" />
+            <div> todo pet</div>
+            <div> 할 일을 완료하며, 펫과 함께 성장하세요.</div>
+
+            <button>구글 아이디로 로그인</button>
+        </Container>
+    );
 }
+
+const Container = styled.div``;
+
+// const MainImage = styled()`
+//     background:
+//         url("@/assets/images/main.png"),
+//         lightgray 50% / cover no-repeat;
+// `;
