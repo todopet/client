1b5ed6016bfc1c7ac3fe95e8fcb7f6868649c392
[SW5] 안보란 <boransama@gmail.com>
2023-08-17 22:38:59 +0900

    fix: 연월일 표시 수정

--- src/components/pages/Todo/Calendar/Week/Week.tsx
+++ src/components/pages/Todo/Calendar/Week/Week.tsx
@@ -2,17 +2,27 @@ import * as Styles from './Week.styles';
 import { ReactComponent as LeftSvg } from "@/assets/images/leftButton.svg";
 import { ReactComponent as RightSvg } from "@/assets/images/rightButton.svg";
 import ArrowButton from '../Button/ArrowButton';
+import { useState } from 'react';
 
 const dayText = ['일', '월', '화', '수', '목', '금', '토'];
 const date = [1, 2, 3, 4, 5, 6, 7];
 
+const today = new Date();
+const thisYear = today.getFullYear();
+const thisMonth = today.getMonth() + 1;
+
+
 export default function Week() {
+    const [currentDay, setCurrentDay] = useState('');
+
     return (
         <Styles.WeekStyle>
             <ArrowButton>
                 <LeftSvg />
             </ArrowButton>
-            <Styles.Title>2022년 7월 2주차</Styles.Title>
+            <Styles.Title>
+                {thisYear}년 {thisMonth}월 N주차
+            </Styles.Title>
             <ArrowButton>
                 <RightSvg />
             </ArrowButton>
