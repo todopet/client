a4e1cb2acbec9cb83195bffe2985bbb47a198648
[SW5] 안보란 <boransama@gmail.com>
2023-08-17 23:00:01 +0900

    feat: 월간캘린더 레이아웃 추가

--- src/components/pages/Todo/Calendar/Month/Month.styles.ts
+++ src/components/pages/Todo/Calendar/Month/Month.styles.ts
@@ -3,7 +3,32 @@ import styled from "styled-components";
 const MonthStyle = styled.div`
     width: 390px;
     height: 390px;
-    background-color: #acc8ff;
 `;
 
-export { MonthStyle };
+const Day = styled.span`
+    margin: 5px 13px;
+    font-size: 14px;
+`;
+
+const CellWrapper = styled.div`
+    display: flex;
+`;
+
+const Cell = styled.div`
+    width: 22px;
+    height: 22px;
+    border-radius: 3px;
+    background-color: lightgray;
+    margin: 4px 10px;
+`;
+
+const Date = styled.span`
+    margin: 5px 17px;
+    font-size: 14px;
+`;
+
+const Title = styled.span`
+    margin: 4px 20px;
+`;
+
+export { MonthStyle, Day, CellWrapper, Cell, Date, Title };
