b126d15404cc27ed153da3b9cd495692ef477c6d
[SW5] 안보란 <boransama@gmail.com>
2023-08-17 14:00:14 +0900

    design: 미니펫 배경 이미지 수정

--- src/components/pages/Todo/MiniPet/MiniPet.styles.ts
+++ src/components/pages/Todo/MiniPet/MiniPet.styles.ts
@@ -1,9 +1,16 @@
 import styled from "styled-components";
 
+
 const MiniPetStyle = styled.div`
     width: 390px;
     height: 50px;
-    background-color: yellow;
+    background-repeat: no-repeat;
+    background-position: center;
+
+    & img {
+        width: 390px;
+        height: 50px;
+    }
 `;
 
 export { MiniPetStyle };
