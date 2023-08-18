74a0b2b80c89fadde4271fcab980ad52d9368bbd
[SW5] 문수민 <sum95771@gmail.com>
2023-08-18 16:11:04 +0900

    feat: 아이템 모달 edit버튼 추가

---
+++ src/components/pages/Pet/Item/EditBtn.styles.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import Button from "@/components/Button/Button";
+
+export interface EditBtnProps {
+    modalType: "useModal" | "discardModal";
+    btnType: "confirm" | "cancel";
+}
+
+const ButtonStyled = styled(Button)<EditBtnProps>`
+    padding: 0;
+    border: none;
+    background-color: transparent;
+    font-size: 20px;
+    font-weight: 500;
+    width: 100%;
+    color: ${(props) =>
+        props.btnType === "confirm"
+            ? "#000000"
+            : props.modalType === "useModal"
+            ? "#2dc770"
+            : "#adadad"};
+`;
+
+const ButtonWrap = styled.div<EditBtnProps>`
+    width: 204px;
+    height: 62px;
+    border-radius: 30px;
+    display: flex;
+    align-items: center;
+    background-color: ${(props) =>
+        props.btnType === "cancel"
+            ? "#ffffff"
+            : props.modalType === "useModal"
+            ? "#aaeea8"
+            : "#adadad"};
+`;
+
+export { ButtonStyled, ButtonWrap };
