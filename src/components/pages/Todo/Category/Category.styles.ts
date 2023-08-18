80ae91b3033de0641d7011f1fbb8aa7f0fe18c8b
[SW5] 안보란 <boransama@gmail.com>
2023-08-18 13:35:28 +0900

    design: 카테고리 디자인 추가

--- src/components/pages/Todo/Category/Category.styles.ts
+++ src/components/pages/Todo/Category/Category.styles.ts
@@ -1,1 +1,31 @@
-export {};
+import styled from "styled-components";
+
+const CategoryStyles = styled.div`
+    background-color: #F2F2F2;
+    width: fit-content;
+    height: 32px;
+    border-radius: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 12px;
+`;
+
+const Text = styled.span`
+    font-size: 14px;
+    font-weight: 600;
+    font-family: Pretendard;
+`;
+
+const PlusButton = styled.button`
+    border-radius: 15px;
+    border: none;
+    background-color: white;
+    width: 20px;
+    height: 20px;
+    padding: 4px 0px;
+    margin-left: 16px;
+    margin-right: 6px;
+`;
+
+export { CategoryStyles, Text, PlusButton };
--- src/components/pages/Todo/Category/Category.tsx
+++ src/components/pages/Todo/Category/Category.tsx
@@ -1,7 +1,15 @@
+import { ReactComponent as PlusSvg } from "@/assets/images/plusButton.svg";
+import { CategoryStyles, Text, PlusButton } from './Category.styles';
+
 export default function Category() {
     return (
         <div>
-            <div>Category</div>
+            <CategoryStyles>
+                <Text>목표1</Text>
+                <PlusButton>
+                    <PlusSvg />
+                </PlusButton>
+            </CategoryStyles>
         </div>
     );
 }
