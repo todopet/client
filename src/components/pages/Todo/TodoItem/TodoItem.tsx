e44865d0dd6dc2b0964ca070627abf56be692fe2
[SW5] 문수민 <sum95771@gmail.com>
2023-08-20 01:10:30 +0900

    feat: todo 목표카테고리 get 요청

--- src/components/pages/Todo/TodoItem/TodoItem.tsx
+++ src/components/pages/Todo/TodoItem/TodoItem.tsx
@@ -1,12 +1,45 @@
-import Category from '../Category/Category';
-import Todos from '../Todos/Todos';
-import { TodoItemStyles } from './TodoItem.styles';
+//react hook
+import { useState, useEffect } from "react";
+
+//api, interface
+import axiosRequest from "@/api/index";
+import { res, category } from "@/@types/index";
+
+//components
+import Category from "../Category/Category";
+import Todos from "../Todos/Todos";
+
+//styles
+import { TodoItemStyles } from "./TodoItem.styles";
 
 export default function TodoItem() {
+    //전체 목표카테고리 get 요청 ->api작업완료시 (전체) -> (날짜)기준으로 바꾸기
+    async function getCategory() {
+        try {
+            const response: res<category[]> = await axiosRequest.requestAxios<
+                res<category[]>
+            >("get", "/todoCategory");
+            console.log(response);
+            setCategory(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    const [category, setCategory] = useState<category[]>();
+    useEffect(() => {
+        getCategory();
+    }, []);
+
     return (
         <TodoItemStyles>
-            <Category />
-            <Todos />
+            {category?.map((el) => {
+                return (
+                    <>
+                        <Category category={el.category} />
+                        <Todos todos={el.todos} />
+                    </>
+                );
+            })}
         </TodoItemStyles>
     );
 }
