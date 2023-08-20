2533ecde0092c9ef0745a5677d36c07ac9f4da0c
[SW5] 이남경 <namkyeong1008@gmail.com>
2023-08-21 00:31:29 +0900

    feat: 뒤로가기, 페이지 별로 다른 목표 title 표시 및 입력하지 않고 버튼 클릭 시 경고 창 띄우기 기능 추가

--- src/pages/Category/CategoryList/CategoryList.tsx
+++ src/pages/Category/CategoryList/CategoryList.tsx
@@ -1,7 +1,32 @@
 import CategoryHeader from "@/components/pages/Category/CategoryHeader/CategoryHeader";
 import CategoryContent from "@/components/pages/Category/CategoryContent/CategoryContent";
+import { res } from "@/@types/index";
+import { useEffect, useState } from "react";
+import axiosRequest from "@/api";
 
 export default function CategoryList() {
+    // 임시로 any 타입을 사용한다.
+    // 타입은 해당 category에 필요한 데이터를 인터페이스로 정의한 뒤 가져온다.
+    // @types폴더에서 파일을 만들고, index.ts로 export 한 다음, index.ts에서 그 데이터를 export 한다.
+    // 그 뒤 현재 페이지에서 import를 하여 사용한다.
+    const [categoryList, setCategoryList] = useState([]);
+    //게시글 get요청
+    async function getPostings() {
+        try {
+            const response: res<any> = await axiosRequest.requestAxios<
+                res<any>
+            >("get", "/todoCategory");
+            // console.log("전체게시글", response.data);
+            console.log(response);
+            setCategoryList(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    useEffect(() => {
+        getPostings();
+    }, []);
+
     return (
         <>
             <CategoryHeader />
