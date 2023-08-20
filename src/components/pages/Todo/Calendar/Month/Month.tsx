12d8c5d31c630262f1ebde9776190b1ede0b7b26
[SW5] 안보란 <boransama@gmail.com>
2023-08-21 00:36:00 +0900

    feat: 월간캘린더 화살표버튼 클릭 시 날짜 변경 기능 추가

--- src/components/pages/Todo/Calendar/Month/Month.tsx
+++ src/components/pages/Todo/Calendar/Month/Month.tsx
@@ -1,12 +1,11 @@
 import * as Styles from "./Month.styles";
 import { ReactComponent as LeftSvg } from "@/assets/images/leftButton.svg";
 import { ReactComponent as RightSvg } from "@/assets/images/rightButton.svg";
-import { ReactComponent as DropdownSvg } from "@/assets/images/dropdownButton.svg";
 import ArrowButton from "../Button/ArrowButton";
-import { useState } from "react";
+import { useState, FC } from "react";
 
 // 오늘을 기준으로 연,월,일,요일을 구함
-// day=요일 date=날짜
+// day=요일 dates=날짜
 const today = new Date();
 const todayYear = today.getFullYear();
 const todayMonth = today.getMonth();
@@ -14,10 +13,21 @@ const todayDay = today.getDay();     // 요일 기준으로 몇번째 칸부터 
 const dayText = ["일", "월", "화", "수", "목", "금", "토"];
 const firstDateOfMonth = new Date(todayYear, todayMonth, 1);
 
+// interface TitleProps {
+//     children?: JSX.Element | JSX.Element[];
+// }
+interface TitleProps {
+    children?: React.ReactNode;
+    year: number;
+    month: number;
+}
+
 export default function Month() {
     const [baseDate, setBaseDate] = useState(firstDateOfMonth);
+    const [month, setMonth] = useState(todayMonth + 1);
+
     // 1일~말일까지의 날짜를 넣을 숫자 배열
-    const date: number[] = [];
+    const dates: number[] = [];
 
     function getDate() {
         const lastDateOfMonth = new Date(
@@ -27,22 +37,54 @@ export default function Month() {
         ).getDate();
 
         for (let i = 1; i <= lastDateOfMonth; ++i) {
-            date.push(i);
+            dates.push(i);
         }
-        console.log(date);
+        console.log(dates);
     }
     getDate();
 
+    const Title: FC<TitleProps> = (props) => {
+        return (
+            <>
+                {props.year}년 {props.month}월
+            </>
+        );
+    }
+
     return (
         <Styles.MonthStyle>
-            <ArrowButton>
+            <ArrowButton
+                onClick={() => {
+                    setBaseDate(
+                        new Date(
+                            baseDate.getFullYear(),
+                            baseDate.getMonth() - 1,
+                            1
+                        )
+                    );
+                    setMonth(baseDate.getMonth() + 1);
+                }}
+            >
                 <LeftSvg />
             </ArrowButton>
             <Styles.Title>
-                {baseDate.getFullYear()}년 {baseDate.getMonth() + 1}월
-                <DropdownSvg width="10px" height="10px" />
+                <Title
+                    year={baseDate.getFullYear()}
+                    month={baseDate.getMonth() + 1}
+                />
             </Styles.Title>
-            <ArrowButton>
+            <ArrowButton
+                onClick={() => {
+                    setBaseDate(
+                        new Date(
+                            baseDate.getFullYear(),
+                            baseDate.getMonth() + 1,
+                            1
+                        )
+                    );
+                    setMonth(baseDate.getMonth() + 1);
+                }}
+            >
                 <RightSvg />
             </ArrowButton>
             <Styles.DayWrap>
@@ -51,10 +93,10 @@ export default function Month() {
                 ))}
             </Styles.DayWrap>
             <Styles.DateCellWrap>
-                {date.map((date, i) => (
+                {dates.map((dates, i) => (
                     <Styles.DateCell>
                         <Styles.Cell></Styles.Cell>
-                        <Styles.Date>{date}</Styles.Date>
+                        <Styles.Date>{dates}</Styles.Date>
                     </Styles.DateCell>
                 ))}
             </Styles.DateCellWrap>
