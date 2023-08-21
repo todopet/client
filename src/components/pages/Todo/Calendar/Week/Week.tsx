1b64b15b81612f8080b457c9349f4bcb7cc54a0b
[SW5] 안보란 <boransama@gmail.com>
2023-08-22 02:41:53 +0900

    fix: 주간캘린더 주차, 월, 날짜 오류 수정

--- src/components/pages/Todo/Calendar/Week/Week.tsx
+++ src/components/pages/Todo/Calendar/Week/Week.tsx
@@ -8,7 +8,7 @@ interface TitleProps {
     children?: React.ReactNode;
     year: number;
     month: number;
-    weekNumber: any;
+    weekCount: number;
 }
 
 // 오늘을 기준으로 연,월,일,요일을 구함
@@ -23,127 +23,124 @@ const dayText = ["일", "월", "화", "수", "목", "금", "토"];
 // 오늘을 기준으로 지난 일요일의 날짜객체를 얻음
 // todayDate - todayDay를 하면 일요일의 날짜가 나오는데, 일요일이 0이기 때문. 일요일~토요일 0~6.
 const lastSunday = new Date(todayYear, todayMonth, todayDate - todayDay);
+let isFirstDateIncluded = false;
+let specialCaseOfYearEnd = false;
 
 export default function Week() {
-    // baseDate를 지난 일요일로 설정, 화살표를 누를 때마다 일주일씩 변경되게 할 것
-    const [baseDate, setBaseDate] = useState(lastSunday);
-    const [month, setMonth] = useState(todayMonth + 1);
-    const [weekNumber, setWeekNumber] = useState(getWeekNumber());
+    const [currentSunday, setCurrentSunday] = useState(lastSunday);
+    const [titleData, setTitleData] = useState({
+        year: currentSunday.getFullYear(),
+        month: currentSunday.getMonth(),
+        weekCount: getWeekCount()
+    });
+    const dates: Date[] = [];
 
-    const date: number[] = [];
-
-    function getWeekDate() {
-        console.log("현재 월: ", baseDate.getMonth() + 1);
-        const lastDateOfMonth = new Date(
-            baseDate.getFullYear(),
-            baseDate.getMonth() + 1,
-            0
-        ).getDate();
-
-        const firstDate = baseDate.getDate();
-
+    const getWeekDates = () => {
         for (let i = 0; i < 7; ++i) {
-            if (firstDate + i - 1 === lastDateOfMonth) {
-                const rest = 7 - date.length;
-                for (let i = 1; i <= rest; ++i) {
-                    date.push(i);
-                }
-                break;
-            }
-            date.push(firstDate + i);
+            const d = new Date(
+                currentSunday.getFullYear(),
+                currentSunday.getMonth(),
+                currentSunday.getDate() + i
+            );
+            dates.push(d);
         }
-        console.log(date);
-    }
-    getWeekDate();
+        isFirstDateIncluded = false;
+        specialCaseOfYearEnd = false;
 
-    function getWeekNumber() {
+        isFirstDateIncluded = dates.map((date) => date.getDate()).includes(1);
+        if (dates[0].getDate() === 1) {
+            // 1일이 일요일인 경우의 예외처리
+            isFirstDateIncluded = false;
+        }
+
+        if (currentSunday.getMonth() === 11) {
+            if (dates.map((date) => date.getDate()).includes(31)) {
+                if (dates[6].getDate() !== 31) {
+                    specialCaseOfYearEnd = true;
+                }
+            }
+        }
+    };
+    getWeekDates();
+
+    function getWeekCount() {
         const firstDayOfMonth = new Date(
-            baseDate.getFullYear(),
-            baseDate.getMonth(),
+            currentSunday.getFullYear(),
+            currentSunday.getMonth(),
             1
         ).getDay();
 
-        const currentDate = baseDate.getDate(); // 20
-        let weekCount = Math.ceil((currentDate + firstDayOfMonth) / 7); // 올림
+        const currentDate = currentSunday.getDate();
+        const weekCount = Math.ceil((currentDate + firstDayOfMonth) / 7);
         return weekCount;
     }
-
-
-    // function isInvalidDate(date: Date) {
-    //     return !date || typeof date !== "object" || !(date instanceof Date);
-    // };
-
-    // function getWeekOfData(date: Date) {
-    //     if (isInvalidDate(date)) return {};
-    //     const firstDate =
-    //         date.getDay() === 0
-    //             ? date
-    //             : new Date(
-    //                 date.getFullYear(),
-    //                 date.getMonth(),
-    //                 date.getDate() - date.getDay()
-    //             );
-
-    //     const lastDate = new Date(
-    //         firstDate.getFullYear(),
-    //         firstDate.getMonth(),
-    //         firstDate.getDate() + 6
-    //     );
-
-    //     const startDayOfMonth = new Date(
-    //         date.getFullYear(),
-    //         date.getMonth()
-    //     ).getDay();
-
-    //     const weekOfMonth: any = Math.ceil((date.getDate() + startDayOfMonth) / 7);
-
-    //     return {
-    //         firstDate, // date 기준 일요일
-    //         lastDate, // date 기준 토요일
-    //         weekOfMonth // date 기준 주차 수 [1 ~ 6]
-    //     };
-    // };
 
     const Title: FC<TitleProps> = (props) => {
         return (
             <>
-                {props.year}년 {props.month}월 {props.weekNumber}주차
+                {props.year}년 {props.month + 1}월 {props.weekCount}주차
             </>
         );
-    }
+    };
+
+    const calculateMonth = () => {
+        if (specialCaseOfYearEnd && isFirstDateIncluded) {
+            console.log("조건1");
+            return 0;
+        } else if (isFirstDateIncluded) {
+            console.log("조건2");
+            return currentSunday.getMonth() + 1;
+        } else {
+            console.log("조건3");
+            return currentSunday.getMonth();
+        }
+    };
+
+    const handleLeftClick = () => {
+        setCurrentSunday(
+            new Date(currentSunday.setDate(currentSunday.getDate() - 7))
+        );
+        setTimeout(() => {
+            setTitleData({
+                year: specialCaseOfYearEnd
+                    ? currentSunday.getFullYear() + 1
+                    : currentSunday.getFullYear(),
+                month: calculateMonth(),
+                weekCount: isFirstDateIncluded ? 1 : getWeekCount()
+            });
+        }, 0);
+    };
+
+    const handleRightClick = () => {
+        setCurrentSunday(
+            new Date(currentSunday.setDate(currentSunday.getDate() + 7))
+        );
+
+        setTimeout(() => {
+            setTitleData({
+                year: specialCaseOfYearEnd
+                    ? currentSunday.getFullYear() + 1
+                    : currentSunday.getFullYear(),
+                month: calculateMonth(),
+                weekCount: isFirstDateIncluded ? 1 : getWeekCount()
+            });
+        }, 0);
+    };
 
     return (
         <Styles.WeekStyle>
             <Styles.TitleWrap>
-                <ArrowButton
-                    onClick={() => {
-                        setBaseDate(
-                            new Date(baseDate.setDate(baseDate.getDate() - 7))
-                        );
-                        // const { weekOfMonth } = getWeekOfData(baseDate);
-                        setMonth(baseDate.getMonth());
-                        setWeekNumber(getWeekNumber());
-                    }}
-                >
+                <ArrowButton onClick={handleLeftClick}>
                     <LeftSvg />
                 </ArrowButton>
                 <Styles.Title>
                     <Title
-                        year={baseDate.getFullYear()}
-                        month={month}
-                        weekNumber={weekNumber}
+                        year={titleData.year}
+                        month={titleData.month}
+                        weekCount={titleData.weekCount}
                     />
                 </Styles.Title>
-                <ArrowButton
-                    onClick={() => {
-                        setBaseDate(
-                            new Date(baseDate.setDate(baseDate.getDate() + 7))
-                        );
-                        // const { weekOfMonth } = getWeekOfData(baseDate);
-                        setMonth(baseDate.getMonth() + 1);
-                        setWeekNumber(getWeekNumber());
-                    }}
-                >
+                <ArrowButton onClick={handleRightClick}>
                     <RightSvg />
                 </ArrowButton>
             </Styles.TitleWrap>
@@ -153,17 +150,64 @@ export default function Week() {
                         <Styles.Day key={i}>{day}</Styles.Day>
                     ))}
                 </Styles.DayWrap>
-                <Styles.CellWrap>
-                    {dayText.map((day, i) => (
-                        <Styles.Cell key={i} />
+                <Styles.DateCellWrap>
+                    {dates.map((date, i) => (
+                        <Styles.DateCell>
+                            <Styles.Cell></Styles.Cell>
+                            <Styles.Date
+                                isToday={
+                                    date.getFullYear() === todayYear &&
+                                    date.getMonth() === todayMonth &&
+                                    date.getDate() === today.getDate()
+                                }
+                            >
+                                {date.getDate()}
+                            </Styles.Date>
+                        </Styles.DateCell>
                     ))}
-                </Styles.CellWrap>
-                <Styles.DateWrap>
-                    {date.map((date, i) => (
-                        <Styles.Date key={i}>{date}</Styles.Date>
-                    ))}
-                </Styles.DateWrap>
+                </Styles.DateCellWrap>
             </div>
         </Styles.WeekStyle>
     );
 }
+
+// 아래 getWeekOfData 함수를 이용해서 아래와 같은 경우에 대해서 코드를 작성해주세요.
+//
+// 해당 Week 컴포넌트 마운트 시점 ( => getWeekOfData( new Date() ) );
+// Arrow 버튼 클릭 시 ( => getWeekOfData( new Date(현재 선택된 날짜 기준 + || - 1) ) )
+//
+// function isInvalidDate(date: Date) {
+//     return !date || typeof date !== "object" || !(date instanceof Date);
+// };
+
+// function getWeekOfData(date: Date) {
+// 매개변수로 Date객체(currentSunday)를 받아옴. getWeekOfDate(currentSunday); 이렇게 호출
+//     if (isInvalidDate(date)) return {};
+//     const firstDate = // 주의 첫날 객체(일요일)
+//         date.getDay() === 0
+//             ? date
+//             : new Date(
+//                 date.getFullYear(),
+//                 date.getMonth(),
+//                 date.getDate() - date.getDay()
+//             );
+
+//     const lastDate = new Date( // 주의 마지막날 객체(토요일)
+//         firstDate.getFullYear(),
+//         firstDate.getMonth(),
+//         firstDate.getDate() + 6
+//     );
+
+//     const startDayOfMonth = new Date( // 월의 시작 요일
+//         date.getFullYear(),
+//         date.getMonth()
+//     ).getDay();
+
+//     const weekOfMonth = Math.ceil((date.getDate() + startDayOfMonth) / 7);
+
+//     return {
+//         firstDate, // date 기준 일요일
+//         lastDate, // date 기준 토요일
+//         weekOfMonth // date 기준 주차 수 [1 ~ 6]
+//     };
+// };
