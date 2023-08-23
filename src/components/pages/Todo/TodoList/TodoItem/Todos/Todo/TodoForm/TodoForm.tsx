f0c9c6d4c9f541868200593281b100181a6b1353
[SW5] 문수민 <sum95771@gmail.com>
2023-08-24 03:02:31 +0900

    fix: 머지 누락사항 롤백

--- src/components/pages/Todo/TodoList/TodoItem/Todos/Todo/TodoForm/TodoForm.tsx
+++ src/components/pages/Todo/TodoList/TodoItem/Todos/Todo/TodoForm/TodoForm.tsx
@@ -73,7 +73,6 @@ export default function TodoForm({
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         submitForm();
-        finishEdit && finishEdit();
     };
 
     //input form 참조
