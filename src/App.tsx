aae18eda8b50ef45def58df0d4cf331fc037b1ad
[SW5] 원종석 <eonjeongcha13@gmail.com>
2023-08-25 17:17:37 +0900

    refactor: route path 로직 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { GlobalStyle } from "@/App.styles";
+import { GlobalStyle, LayoutWrapper } from "@/App.styles";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "@/components/layout/Footer/Footer";
+import Header from "@/components/layout/Header/Header";
 import Todo from "@/pages/Todo/Todo";
 import Login from "@/pages/Login/Login";
-import CommonLayout from "./components/layout/CommonLayout";
 import Ranking from "@/pages/Ranking/Ranking";
 import Pet from "@/pages/Pet/Pet";
 import MyPage from "@/pages/MyPage/MyPage";
@@ -11,28 +12,45 @@ import CategoryList from "@/pages/Category/CategoryList/CategoryList";
 import CategoryPost from "@/pages/Category/CategoryPost/CategoryPost";
 // import NotFound from "@/pages/NotFound";
 
+const routePaths = [
+    { path: "/", element: <Login />, withHeader: false, withFooter: false },
+    {
+        path: "/category/list",
+        element: <CategoryList />,
+        withHeader: false,
+        withFooter: false
+    },
+    {
+        path: "/category/post",
+        element: <CategoryPost />,
+        withHeader: false,
+        withFooter: false
+    },
+    { path: "/todo", element: <Todo />, withHeader: true, withFooter: true },
+    { path: "/pet", element: <Pet />, withHeader: true, withFooter: true },
+    { path: "/rank", element: <Ranking />, withHeader: true, withFooter: true },
+    { path: "/mypage", element: <MyPage />, withHeader: true, withFooter: true }
+];
+
 const App: React.FC = () => {
     return (
         <>
             <GlobalStyle />
             <Router>
-                <CommonLayout>
                     <Routes>
-                        <Route path="/" element={<Login />} />
-                        <Route
-                            path="/category/list"
-                            element={<CategoryList />}
-                        />
-                        <Route
-                            path="/category/post"
-                            element={<CategoryPost />}
-                        />
-                        <Route path="/todo" element={<Todo />} />
-                        <Route path="/pet" element={<Pet />} />
-                        <Route path="/rank" element={<Ranking />} />
-                        <Route path="/mypage" element={<MyPage />} />
+                        {routePaths.map((data) => (
+                            <Route
+                                path={data.path}
+                                element={
+                                    <LayoutWrapper>
+                                        {data.withHeader && <Header />}
+                                        {data.element}
+                                        {data.withFooter && <Footer />}
+                                    </LayoutWrapper>
+                                }
+                            />
+                        ))}
                     </Routes>
-                </CommonLayout>
             </Router>
         </>
     );
