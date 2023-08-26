e86fe56ac9761406176cd7a536f4292b74faba01
JAEUKJEON <jojeon4515@gmail.com>
2023-08-27 01:14:52 +0900

    fix: 아이템 사용 api 호출 로직 수정

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -4,10 +4,11 @@ import { todo } from "@/@types/todo";
 import { myPet } from "@/@types/myPet";
 import { myItems } from "./myItems";
 import { dumpItemRes } from "@/@types/dumpItemRes";
+import { useItemRes } from "./useItemRes"; 
 
 interface res<D> {
     error: string | null;
     data: D;
 }
 
-export type { res, user, myPet, category, todo, myItems, todoCategory, dumpItemRes };
+export type { res, user, myPet, category, todo, myItems, todoCategory, dumpItemRes, useItemRes };
