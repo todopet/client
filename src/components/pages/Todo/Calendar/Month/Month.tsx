8338bbddd51b21ddd54a4e7fa812c9ec37bd6bab
[SW5] 안보란 <boransama@gmail.com>
2023-08-29 01:15:47 +0900

    feat: 월간캘린더 Cell(잔디)컬러 나타내는 기능 추가

--- src/components/pages/Todo/Calendar/Month/Month.tsx
+++ src/components/pages/Todo/Calendar/Month/Month.tsx
@@ -5,19 +5,13 @@ import ArrowButton from "../Button/ArrowButton";
 import React, { useState, FC, useContext, useEffect } from "react";
 import { TodoContext } from "@/components/pages/Todo/TodoContext";
 
-interface TitleProps {
-    children?: React.ReactNode;
-    year: number;
-    month: number;
-}
-
-// 오늘을 기준으로 연,월,일,요일을 구함
-// day=요일 dates=날짜
+// 오늘의 연,월,일,요일 구하기. day=요일 date=날짜
 const today = new Date();
 const todayYear = today.getFullYear();
 const todayMonth = today.getMonth();
-const todayDate = today.getDate();
 const dayText = ["일", "월", "화", "수", "목", "금", "토"];
+
+// 오늘을 기준으로 이번 달의 1일의 날짜 객체를 얻음
 const firstDateOfMonth = new Date(todayYear, todayMonth, 1);
 
 export default function Month() {
@@ -25,12 +19,18 @@ export default function Month() {
     const [month, setMonth] = useState(todayMonth + 1);
     const [clicked, setClicked] = useState(-1);
 
-    // 한달의 날짜를 넣는 배열
-    const dates: Date[] = [];
-    const todoCountForCell: number[] = [];
+    const dates: Date[] = []; // 한달의 날짜를 넣는 배열
+    const completedTodosByDay: number[] = [];
+    const { updateSelectedDate, getTodos, periodTodos } =
+        useContext(TodoContext);
 
-    // Calendar로부터 전달받음
-    const { updateDate, periodTodos } = useContext(TodoContext);
+    // 날짜 형식 yyyy-mm-dd 지정 함수
+    const formatDate = (date: Date) => {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, "0");
+        const day = String(date.getDate()).padStart(2, "0");
+        return `${year}-${month}-${day}`;
+    };
 
     function getMonthDate() {
         const firstDateOfMonth = new Date(
@@ -44,6 +44,7 @@ export default function Month() {
             baseDate.getMonth() + 1,
             0
         );
+
         for (let i = 0; i < firstDateOfMonth.getDay(); ++i) {
             dates.push(new Date(9999, 11, 31));
         }
@@ -52,24 +53,45 @@ export default function Month() {
                 new Date(baseDate.getFullYear(), baseDate.getMonth(), 1 + i)
             );
         }
-
-        const formatDate = (date: Date) => {
-            const year = date.getFullYear();
-            const month = String(date.getMonth() + 1).padStart(2, "0");
-            const day = String(date.getDate()).padStart(2, "0");
-
-            return `${year}-${month}-${day}`;
-        };
-
-        const start = formatDate(dates[baseDate.getDay()]);
-        const end = formatDate(dates[dates.length - 1]);
-        updateDate(start, end);
     }
+
+    const countDates = () => {
+        const todoDates: number[] = []; // todo들을 평탄화한 날짜 배열
+        // console.log(periodTodos);
+        periodTodos?.forEach((category: any) =>
+            category.todos.forEach((todo: any) => {
+                const newDate = new Date(todo.createdAt);
+                todoDates.push(newDate.getDate());
+            })
+        );
+        // console.log("Month todoDates: ", todoDates);
+
+        for (let i = 0; i < dates.length; ++i) {
+            let count = 0;
+            for (let j = 0; j < todoDates.length; ++j) {
+                if (todoDates[j] === i - 1) {
+                    ++count;
+                }
+            }
+            completedTodosByDay.push(count);
+        }
+        // console.log("Month completedTodosByDay배열: ", completedTodosByDay);
+    };
+
     getMonthDate();
-    // useEffect(() => { getMonthDate() }, [baseDate]);
+    countDates();
+
+    const start = formatDate(dates[baseDate.getDay()]);
+    const end = formatDate(dates[dates.length - 1]);
+
+    useEffect(() => {
+        getTodos(start, end);
+    }, [baseDate]);
 
     const handleDateCellClick = (idx: number) => {
         setClicked(idx ?? -1);
+        updateSelectedDate(formatDate(dates[idx]));
+        // console.log(idx);
     };
 
     return (
@@ -118,7 +140,7 @@ export default function Month() {
                         <Styles.DateCell></Styles.DateCell>
                     ) : (
                         <Styles.DateCell onClick={() => handleDateCellClick(i)}>
-                            <Styles.Cell />
+                            <Styles.Cell completed={completedTodosByDay[i]} />
                             <Styles.Date
                                 id={i}
                                 isToday={
