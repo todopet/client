7e71a89768c857ba29f6adb074d49bd82a3ad7af
JAEUKJEON <jojeon4515@gmail.com>
2023-08-30 23:12:57 +0900

    design: 펫 화면 수정 fix: 아이템 사용,버리기 api 로직, 인터페이스 수정

--- src/@types/dumpItemRes.ts
+++ src/@types/dumpItemRes.ts
@@ -1,4 +1,13 @@
 export interface dumpItemRes {
-    status: number;
-    message: string;
+    createdAt: string;
+    updatedAt: string;
+    userId: string;
+    _id: string;
+    items: item[];
+}
+
+interface item {
+    item: string;
+    quantity: number;
+    _id: string;
 }
