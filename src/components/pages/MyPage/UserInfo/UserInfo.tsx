ee6f9e022262f9778e074a6ae72aeb1a3efc8e3e
JAEUKJEON <jojeon4515@gmail.com>
2023-09-07 15:32:26 +0900

    feat: 닉네임 변경시 문구 출력, 아이템 모달 코드 개선, catch문 alert 추가

--- src/components/pages/MyPage/UserInfo/UserInfo.tsx
+++ src/components/pages/MyPage/UserInfo/UserInfo.tsx
@@ -39,66 +39,44 @@ export function UserInfo({ picture, name, date }: userinfoType) {
             >("get", "/users/user");
             setNickname(response.data.nickname);
         } catch (error) {
-            alert("오류가 발생했습니다. 다시 시도해 주세요.");
+            alert("회원 정보를 가져오는중 에러가 발생했습니다. 다시 시도해 주세요.");
+            console.log("Error fetching pet data: ", error);
         }
     };
-
-    // const changeNickname = (e: any) => {
-    //     const inputValue = e.target.value;
-    //     setNickname(inputValue);
-
-    //      // 유효성 검사를 수행합니다.
-    //     if (/[^a-zA-Z0-9가-힣]/.test(nickname) || nickname.length > 12) {
-    //         setError('닉네임은 12글자 이하의 영문, 숫자, 한글로 이루어져야 합니다.');
-    //     } else {
-    //         setError('');
-    //     }
-
-    //     console.log(nickname);
-    //     console.log(error);
-    // }
-
-    // useEffect(() => {
-    //     // 상태가 변경될 때마다 실행되는 코드
-    //     if (nickname.length > 6) {
-    //         setError('닉네임은 6글자 이하여야 합니다.');
-    //     } else {
-    //         setError('');
-    //     }
-    // }, [nickname]);
-
-    const [charCount, setCharCount] = useState(0);
 
     useEffect(() => {
-        if (charCount > 6) {
-        setError('닉네임은 한글 6자, 영어 12자까지 입력 가능합니다.');
+        // 상태가 변경될 때마다 실행되는 코드
+        const hasWhitespace = /\s/.test(nickname);
+
+        if (hasWhitespace) {
+            setError('공백은 포함될수 없습니다.');
+            return;
+        }
+        if (nickname.length > 8) {
+            setError('닉네임은 8글자 이하여야 합니다.');
+            return;
+        } 
+        if (nickname.length > 0) {
+            setError('올바른 닉네임입니다.');
         } else {
-        setError('');
+            setError('닉네임을 입력하세요.');
         }
-    }, [charCount]);
+    }, [nickname]);
 
-    const changeNickname = ({ target }: ChangeEvent<HTMLInputElement>) => {
-        const { value } = target;
-        // const inputValue = e.target.value;
-        setNickname(value);
+    const inputColor = () => {
+        if (error === "올바른 닉네임입니다.") return "#bff2bd";
+        if (error === "닉네임을 입력하세요.") return "#d1d1d1";
+        return "#FF5656";
+    }
 
-        let count = 0;
-        for (let i = 0; i < value.length; i++) {
-            // 한글 여부를 판단하여 카운트 증가
-            if (/[ㄱ-ㅎ가-힣]/.test(value[i])) {
-                count += 1;
-            } 
-            if (/[a-zA-Z]/.test(value[i])) {
-                count += 0.5;
-            }
-        }
-
-        setCharCount(count);
-    };
+    const textColor = () => {
+        if (error === "올바른 닉네임입니다.") return "#8afd85";
+        if (error === "닉네임을 입력하세요.") return "#ababab";
+        return "#ff1919";
+    }
 
     console.log(nickname);
     console.log(error);
-    console.log(charCount);
 
     const handleClick = () => {
         getUserName();
@@ -114,8 +92,8 @@ export function UserInfo({ picture, name, date }: userinfoType) {
             const response: res<myUser> = await axiosRequest.requestAxios<
                 res<myUser>
             >("patch", "/users/myInfo", { nickname });
-
-            if(response.data) {  // 닉네임이 제대로 변경이 되었을 때
+            console.log(response);
+            if (response.data) {  // 닉네임이 제대로 변경이 되었을 때
                 alert("닉네임이 수정되었습니다!");
             }
             else {  // 글자 제한을 통과하지 못해 변경이 되지 않았을때
@@ -123,9 +101,11 @@ export function UserInfo({ picture, name, date }: userinfoType) {
             }
             navigate(0);  // 페이지 새로고침
         } catch (error) {
-            alert("오류가 발생했습니다. 다시 시도해 주세요.");
+            alert("닉네임 변경중 에러가 발생했습니다. 다시 시도해 주세요.");
+            console.log("Error fetching pet data: ", error);
         }
     };
+
     return (
         <UserInfoWrapper>
             <UserIcon imagepath={picture}></UserIcon>
@@ -144,13 +124,13 @@ export function UserInfo({ picture, name, date }: userinfoType) {
                                     <ModalInput
                                         type="text"
                                         value={nickname}
-                                        onChange={changeNickname}
-                                        // onChange={(e: any) =>
-                                        //     setNickname(e.target.value)
-                                        // }
-                                        style={{ borderColor: error ? 'red' : '' }}
+                                        // onChange={changeNickname}
+                                        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                                            setNickname(e.target.value)
+                                        }
+                                        style={{ borderColor: inputColor() }}
                                     />
-                                    <ErrorText error={error}>{error}</ErrorText>
+                                    <ErrorText color={textColor()}>{error}</ErrorText>
                                     {/* {error && <ErrorText>{error}</ErrorText>} */}
                                 </ModalInputArea>
                                 <ModalButtonArea>
