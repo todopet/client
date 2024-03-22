e2f41e78a7319d4773ce0de1f772aac88bd1f8a1
[SW5] 문수민 <sum95771@gmail.com>
2024-03-22 23:54:09 +0900

    refactor: 렌더링 최적화(reflow 줄이기)

--- src/components/pages/Todo/MiniPet/MiniPet.styles.ts
+++ src/components/pages/Todo/MiniPet/MiniPet.styles.ts
@@ -1,4 +1,24 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const jump = keyframes`
+0% {
+  transform: translateY(0px)
+}
+7% {
+  transform: translateY(0px)
+}
+100% {
+  transform: translateY(-20px)
+}
+`;
+const move = keyframes`
+0% {
+  transform: translateX(0px)
+}
+100% {
+  transform: translateX(390px)
+}
+`;
 
 const MiniPetWrap = styled.div`
     width: 390px;
@@ -22,18 +42,20 @@ const MyPet = styled.div<{
     background-repeat: no-repeat;
     background-position: center;
     background-size: contain;
-    position: absolute;
     width: ${(props) => props.width}px;
     height: ${(props) => props.height}px;
 
+    animation: ${jump} 0.6s alternate infinite;
+`;
+const MyPetWrap = styled.div`
     position: absolute;
     z-index: 1;
     bottom: 5px;
+    animation: ${move} 10s linear infinite;
 `;
-
 const Bg = styled.img`
     width: 390px;
     height: 50px;
 `;
 
-export { MiniPetWrap, Bg, MyPet };
+export { MiniPetWrap, Bg, MyPet, MyPetWrap };
