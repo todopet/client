c0e2a70c16fc9660c659954ab647d195e8395ac2
rebi13 <rebi13@naver.com>
2025-11-25 00:53:28 +0900

    Refactor: vite 마이그레이션 / tailwindcss 세팅 (#45)

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
@@ -14,7 +13,4 @@ root.render(
     </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 성능 측정이 필요한 경우 Web Vitals 도입을 별도로 검토하세요.
