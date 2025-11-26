87993be800025efa7644849b6a80683709dbd4ab
rebi13 <rebi13@naver.com>
2025-11-26 23:33:12 +0900

    chore: 세팅 변경 (#45)

--- vite.config.ts
+++ vite.config.ts
@@ -3,50 +3,39 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
-import { visualizer } from "rollup-plugin-visualizer";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import path from "path";
 
 export default defineConfig({
-    plugins: [
-        react(),
-        tsconfigPaths(),
-        svgr({ include: "**/*.svg" }),
-        nodePolyfills({
-            protocolImports: true // 브라우저용 stream, buffer 등 자동 대응
-        })
-    ],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "src")
-        }
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr({ include: "**/*.svg" }),
+    nodePolyfills({
+      protocolImports: true, // 브라우저용 stream, buffer 등 자동 대응
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
-    define: {
+  },
+  define: {
+    global: "globalThis",
+    "process.env": {}, // 일부 라이브러리 대응
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
         global: "globalThis",
-        "process.env": {} // 일부 라이브러리 대응
+      },
     },
-    optimizeDeps: {
-        esbuildOptions: {
-            define: {
-                global: "globalThis"
-            }
-        }
-    },
-    build: {
-        outDir: "build",
-        rollupOptions: {
-            plugins: [
-                visualizer({
-                    filename: "build/stats.html", // ✅ 여기에 생성됨
-                    open: true, // ✅ 자동 열기 (선택)
-                    gzipSize: true,
-                    brotliSize: true
-                })
-            ]
-        }
-    },
-    server: {
-        host: true,
-        port: 3000
-    }
+  },
+  build: {
+    outDir: "build",
+  },
+  server: {
+    host: true,
+    port: 3000,
+  },
 });
